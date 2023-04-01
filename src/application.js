ca969523ee549f5017d79fd8caa2ea56cc0aacaf
Elena Korchuganova <korchel21@gmail.com>
2023-04-01 16:06:23 +0300

    step 3

--- src/application.js
+++ src/application.js
@@ -1,6 +1,16 @@
-import { string } from 'yup';
+import { string, setLocale } from 'yup';
 import onChange from 'on-change';
+import i18n from 'i18next';
 import render from './view/render.js';
+import texts from './resources.js';
+
+// https://rt.com/rss/news
+
+// http://www.dp.ru/exportnews.xml
+
+// http://www.fontanka.ru/fontanka.rss
+
+// http://lenta.ru/l/r/EX/import.rss
 
 const state = {
   form: {
@@ -11,29 +21,45 @@ const state = {
 
 };
 
-const schema = string().url('Ссылка должна быть валидным URL').notOneOf(state.form.feeds, 'RSS уже существует');
+const form = document.querySelector('.rss-form');
+const watchedState = onChange(state, (path, value) => render(state, form));
+
+const validateForm = (i18nextInstance, url) => {
+  setLocale({
+    string: {
+      url: i18nextInstance.t('validation.errors.url'),
+      notOneOf: i18nextInstance.t('validation.errors.notOneOf'),
+    },
+  });
+  const schema = string().url().notOneOf(state.form.feeds);
+  schema.validate(url)
+    .then((result) => {
+      state.form.feeds.push(result);
+      state.form.error = '';
+      watchedState.form.valid = true;
+    })
+    .catch((err) => {
+      const [error] = err.errors;
+      state.form.error = error;
+      watchedState.form.valid = false;
+    });
+};
 
 const app = () => {
-  const form = document.querySelector('.rss-form');
-
-  const watchedState = onChange(state, (path, value) => render(state, form));
+  const i18nextInstance = i18n.createInstance();
+  i18nextInstance.init({
+    lng: 'ru',
+    debug: true,
+    resources: {
+      ru: texts,
+    },
+  });
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const url = formData.get('url');
-    schema.validate(url)
-      .then((result) => {
-        state.form.feeds.push(result);
-        state.form.error = '';
-        watchedState.form.valid = true;
-      })
-      .catch((err) => {
-        const [error] = err.errors;
-        state.form.error = error;
-        watchedState.form.valid = false;
-      });
-    console.log(state);
+    validateForm(i18nextInstance, url);
   });
 };
 
---
+++ src/resources.js
@@ -0,0 +1,10 @@
+export default {
+  translation: {
+    validation: {
+      errors: {
+        url: 'Ссылка должна быть валидным URL',
+        notOneOf: 'RSS уже существует',
+      },
+    },
+  },
+};
